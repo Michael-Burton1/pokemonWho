8e99df4e63aecacf71ade9c3f8df0969c502025c
Michael Burton <michaelburton@Michaels-MBP.domain>
2022-02-18 11:27:21 -0800

    add scripts to display pokemon silouette

--- public/index.js
+++ public/index.js
@@ -1,10 +1,15 @@
 let gameData;
 const main =document.querySelector('main');
 const playBtn =document.querySelector('#play');
+const PokemopnImage = document.querySelector('#pokemon-image')
 
 playBtn.addEventListener('click', fetchData);
 
 async function fetchData(){
-  console.log('clicked');
   gameData = await window.getPokeData();
+  showSil();
+}
+
+function showSil() {
+  PokemopnImage.src = gameData.correct.image
 }
