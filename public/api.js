8cec5ea14f1ae750bf2e3989d20ab59525945ea7
Michael Burton <michaelburton@Michaels-MBP.domain>
2022-02-18 11:25:52 -0800

    add scripts get image of pokemon

--- public/api.js
+++ public/api.js
@@ -2,7 +2,19 @@ window.getPokeData =async function(){
   const pokemon = await getPokemon();
   const randomPokemon = shuffle(pokemon);
   const pokemonChoices = get4Pokemon(randomPokemon);
-  console.log(pokemonChoices);
+  const [ firstPokemon ]= pokemonChoices;
+  const number = getPokemonNumber(firstPokemon)
+  const image = getPokemonImage(number)
+  console.log(image);
+  console.log(firstPokemon)
+
+  return {
+    pokemonChoices,
+    correct: {
+      image,
+      name: firstPokemon.name 
+    }
+  }
 }
 
 async function getPokemon(){
@@ -21,4 +33,13 @@ function shuffle(unshuffled){
 
 function get4Pokemon(randomPokemon){
   return randomPokemon.splice(0, 4);
+}
+function getPokemonNumber({url}){
+const numberRegEx = /(\d+)\/$/;
+return (url.match(numberRegEx) || [])[1];
+
+}
+
+function getPokemonImage(number){
+  return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${number}.png`
 }
