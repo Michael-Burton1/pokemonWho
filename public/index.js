3c03c6543c80cc57a1523ade95e45f8884741cca
Michael Burton <michaelburton@Michaels-MBP.domain>
2022-02-18 11:41:58 -0800

    add scripts to display choices buttons

--- public/index.js
+++ public/index.js
@@ -2,14 +2,25 @@ let gameData;
 const main =document.querySelector('main');
 const playBtn =document.querySelector('#play');
 const PokemopnImage = document.querySelector('#pokemon-image')
+const choices = document.querySelector('#choices');
 
 playBtn.addEventListener('click', fetchData);
 
 async function fetchData(){
   gameData = await window.getPokeData();
   showSil();
+  displayChoices();
 }
 
 function showSil() {
   PokemopnImage.src = gameData.correct.image
+}
+
+function displayChoices(){
+  const { pokemonChoices } = gameData;
+  const choicesHTML = pokemonChoices.map(({name}) =>{
+    return `<button data-name= "${name}">${name}</button>`
+  }).join('');
+
+  choices.innerHTML =choicesHTML;
 }
